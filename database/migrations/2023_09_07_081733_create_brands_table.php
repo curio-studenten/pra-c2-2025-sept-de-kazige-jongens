02db32f4bfdb23f01b19aa96c388decd82bb986a
royboot <rgrizell467@gmail.com>
2025-09-21 12:53:51 +0200

    added a contact number to the create brands migration to be able to contact brands for any needed info

--- database/migrations/2023_09_07_081733_create_brands_table.php
+++ database/migrations/2023_09_07_081733_create_brands_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer("brand_contact_number")->nullable()->default(null);
             $table->timestamps();
         });
     }
